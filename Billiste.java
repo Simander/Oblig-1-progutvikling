0daf469165183f7b9c758d745c04012fd595c96d
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-02-11 13:49:38 +0100

    Update Billiste.java

--- Billiste.java
+++ Billiste.java
@@ -23,5 +23,21 @@ public class Billiste
 			runner.next = neu;
 		}
 	}
+	//Method for finding a car in the list by registration number
+	public Bil findCar( String regNum )
+	{
+		Bil runner = first;
+		while ( runner.next.getRegNum() != null && runner.next.getRegNum() == regNum )
+			runner = runner.next;
+		return runner.next;
+	}
+	//Method for removing a car from the list
+	public void delCar( String regNum )
+	{
+		Bil runner = first;
+		while ( runner.next.getRegNum() != null && runner.next.getRegNum() == regNum )
+			runner = runner.next;
+		runner.next = runner.next.next;
+	}
 	
 }
