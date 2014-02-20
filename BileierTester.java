1f46431d66104d6c39ceecf1bb76dc8f22e7cf46
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-02-20 17:41:22 +0100

    Update BileierTester.java

--- BileierTester.java
+++ BileierTester.java
@@ -9,6 +9,8 @@ public class BileierTester {
 	public static void main(String[] args) 
 	{
 		final BileierRegister bGUI = new BileierRegister();
+		bGUI.setVisible(true);
+		bGUI.setSize(320, 480);
 		bGUI.addWindowListener( new WindowAdapter()
 		{
 			public void windowClosing ( WindowEvent e)
