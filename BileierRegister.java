d9cbce2f2be275cb57aeb5384580bcc0df2bd933
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-02-23 14:13:13 +0100

    Update BileierRegister.java

--- BileierRegister.java
+++ BileierRegister.java
@@ -9,29 +9,37 @@ public class BileierRegister extends JFrame
 	Bileierliste carOwner;
 	private Commandlistener lytter;
 	private JTextField uNr;
+	private JTextField regNr;
 	private JTextField name;
 	private JTextField add;
 	private JButton find;
 	private JButton regO;
 	private JButton delO;
 	private JTextArea info;
+	private JCheckBox check;
 	
 	public BileierRegister()
 	{
 		super("Bileier register");
-		uNr = new JTextField(25);
-		name = new JTextField(25);
-		add = new JTextField(25);
+		uNr = new JTextField(35);
+		check = new JCheckBox("", null, false);
+		//check = new JCheckBox("Person", null, false);
+		regNr = new JTextField(30); 
+		name = new JTextField(30);
+		add = new JTextField(27);
 		find = new JButton("finn");
-		regO = new JButton("Registrer ny neier");
+		regO = new JButton("Registrer ny eier");
 		delO = new JButton("Slett eier");
-		
+		info = new JTextArea(18, 32);
 		//Opretter et content pane
 		Container c = getContentPane();
 		c.setLayout( new FlowLayout() );
 		//Legger elementer til ContentPane
-		c.add(new JLabel( "Personnummer/Foretaksnummer:"));
+		c.add(new JLabel( "Personnummer/Kryss av hvis foretaksnummer"));
+		c.add(check);
 		c.add(uNr);
+		c.add(new JLabel( "RegNr:" ));
+		c.add(regNr);
 		c.add(new JLabel( "Navn: "));
 		c.add(name);
 		c.add(new JLabel( "Addresse: "));
@@ -49,11 +57,33 @@ public class BileierRegister extends JFrame
 		regO.addActionListener(lytter);
 		delO.addActionListener(lytter);
 	}
+	private void insertCarOwner()
+	{
+		int num = Integer.parseInt(uNr.getText());
+		Bileier usr;
+		String n = name.getText();
+		String a = add.getText();
+		if(carOwner.find1(num) == null)
+		{
+			if (check.isSelected()==false)
+			{
+				usr = new Person(n, a);
+				carOwner.insertBileier(usr);
+				JOptionPane.showMessageDialog(null, "ny eier, type person er registrert!");
+			}
+			else
+				usr = new Firma(n, a);
+				carOwner.insertBileier(usr);
+		}		JOptionPane.showMessageDialog(null, "ny eier, type firma er registrert!");
+		if (carOwner.find1(num) != null)
+				JOptionPane.showMessageDialog(null, "eieren er allerede registrert!");
+	}
 	private void showErrorMsg(String message)
 	  {
 	    JOptionPane.showMessageDialog(this, message, 
 	            "Problem", JOptionPane.ERROR_MESSAGE);
 	  }
+	//method for reading serialized objects from file
 	private void readFile()
 	{
 		try (ObjectInputStream inFile = new ObjectInputStream(
@@ -78,6 +108,7 @@ public class BileierRegister extends JFrame
 	      carOwner = new Bileierliste();
 	    }
 	  }
+	//method for writing serialized data to file
 	public void writeToFile()
 	{
 		try (ObjectOutputStream outFile = new ObjectOutputStream(
@@ -101,11 +132,11 @@ public class BileierRegister extends JFrame
 	    {
 			if ( e.getSource() == find )
 		    {
-		        
+		        carOwner.findOwner(regNr.getText());
 		    }
 			else if ( e.getSource() == regO )
 			{
-				
+				insertCarOwner();
 			}
 			else if ( e.getSource() == delO )
 			{
