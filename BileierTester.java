5ab06fd5e0e899c4638acd4a0e1b3823718bb65a
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-02-23 14:14:14 +0100

    Create BileierTester.java

---
+++ BileierTester.java
@@ -0,0 +1,25 @@
+package bileier;
+
+import java.awt.event.*;
+public class BileierTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		final BileierRegister bGUI = new BileierRegister();
+		bGUI.setVisible(true);
+		bGUI.setSize(400, 480);
+		bGUI.addWindowListener( new WindowAdapter()
+		{
+			public void windowClosing ( WindowEvent e)
+			{ 
+				bGUI.writeToFile();
+				System.exit(0);
+			}
+		});
+
+	}
+
+}
