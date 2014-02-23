e09fe42fda119ab878c8c1318ecde4f5225a78e9
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-02-23 14:16:27 +0100

    Update Bileierliste.java

--- Bileierliste.java
+++ Bileierliste.java
@@ -2,6 +2,7 @@ package bileier;
 
 import java.io.Serializable;
 
+import bil.Bil;
 import bil.Billiste;
 
 public class Bileierliste implements Serializable
@@ -26,6 +27,51 @@ public class Bileierliste implements Serializable
 			runner.next = neu;
 		}
 	}
+	//Method for finding a owner based on personalnumber/businessnumber
+	public Bileier find1( int usNum )
+	{
+			
+		Bileier runner = first;
+		if(runner instanceof Person )
+			
+		if (runner != null)
+		{
+			if(runner instanceof Person)
+			{
+				Person usr = (Person)runner;
+				if (usr.getpNr() == usNum)
+					return runner;
+			}
+			else if (runner instanceof Firma )
+			{
+				Firma usr = (Firma)runner;
+				if ( usr.getbNr() == usNum)
+					return runner;
+			}
+		}
+		else
+		{
+			while ( runner.next != null)
+			{
+				if(runner.next instanceof Person)
+				{
+					Person usr = (Person)runner.next;
+					if(usr.getpNr() == usNum)
+						return usr;
+				}
+				else if( runner.next instanceof Firma )
+				{
+					Firma usr = (Firma)runner.next;
+					if ( usr.getbNr() == usNum)
+						return usr;
+				}
+				else
+					runner = runner.next;
+			}
+		}
+		return runner.next;
+	}
+	
 	//Method for finding a owner based on owned cars registration number
 		public Bileier findOwner( String regNum )
 		{
